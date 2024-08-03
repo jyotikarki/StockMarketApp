13d172509b5a7b4fa1a0dde008cf9db1b5d6b670
jojo <jyotidkarki@gmail.com>
2024-08-03 12:40:57 +0530

    main

--- StockMarketUI-main/StockMarketUI-main/utils/axiosInstance.ts
+++
@@ -1,9 +0,0 @@
-// utils/axiosInstance.ts
-import axios from 'axios';
-
-const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000', // Change this to your Django backend URL
-});
-
-export default axiosInstance;
-
